dfe45afb822dd3fd7a353641004e7a62fdf4fc07
iconde <iconde@Ubuntu20.04.myguest.virtualbox.org>
2023-09-26 13:59:02 +0200

    Assignment 7 part 1

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -29,9 +29,41 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
+    size_t total_chars = 0;
+    size_t start_entry = buffer->out_offs;
+
+    // Loop through the entries starting from buffer->out_offs
+    for (size_t i = start_entry; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED + start_entry; ++i)
+    {
+        // Calculate the index taking into account wrapping around to the beginning
+        size_t index = i % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+
+        // Check if the entry at this index is empty
+        if (buffer->entry[index].buffptr == NULL)
+        {
+            // If we've searched all entries, exit the loop
+            if (i == start_entry + AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED - 1)
+            {
+                break;
+            }
+            continue;
+        }
+
+        // Calculate the total chars including the current entry
+        total_chars += buffer->entry[index].size;
+
+        // Check if the char_offset is within this entry
+        if (char_offset < total_chars)
+        {
+            // Calculate the byte offset within the entry
+            *entry_offset_byte_rtn = char_offset - (total_chars - buffer->entry[index].size);
+
+            // Return a pointer to the corresponding entry structure
+            return &buffer->entry[index];
+        }
+    }
+
+    // If char_offset is not found in any entry, return NULL
     return NULL;
 }
 
@@ -44,9 +76,27 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+    // If the buffer is full, overwrite the oldest entry and advance buffer->out_offs
+    if (buffer->full)
+    {
+        // Mark the oldest entry as unused
+        buffer->entry[buffer->out_offs].buffptr = NULL;
+        buffer->entry[buffer->out_offs].size = 0;
+        // Advance out_offs to the next index
+        buffer->out_offs = (buffer->out_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+
+    // Copy data from the new entry to the current input position and advance in_offs
+    buffer->entry[buffer->in_offs].buffptr = add_entry->buffptr;
+    buffer->entry[buffer->in_offs].size = add_entry->size;
+    // Advance in_offs to the next index
+    buffer->in_offs = (buffer->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+
+    // Mark the buffer as full if in_offs reaches out_offs
+    if (buffer->in_offs == buffer->out_offs)
+    {
+        buffer->full = true;
+    }
 }
 
 /**
